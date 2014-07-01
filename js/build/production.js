2e6360888bfc03e07ca417d31f52f437e92b4c8f
quattromani <max@maxquattromani.com>
2014-07-01 17:04:32 -0600

    added home nav, moved locations, company profile page, sticky footer

--- js/build/production.js
+++ js/build/production.js
@@ -371,3 +371,31 @@ $(function() {
   // ... and in inline code
   alxPrc.replaceERBTags($('p code'));
 });
+
+/* ==========================================================================
+    Sticky -- Version: 0.2.1
+   ========================================================================== */
+
+if (!!$('.sticky').offset()) {
+	$(function(){
+		var stickyTop = $('.sticky').offset().top;
+
+		var stickyNav = function(){
+		var scrollTop = $(window).scrollTop();
+
+		if (scrollTop > stickyTop){
+			$('.sticky').addClass('js-fixed');
+			$('.spacer').css({'height':$('.sticky').height()+'px'});
+		} else {
+			$('.sticky').removeClass('js-fixed');
+			$('.spacer').css({'height':'0px'});
+		}
+	};
+
+	stickyNav();
+
+		$(window).scroll(function(){
+			stickyNav();
+		});
+	});
+}
