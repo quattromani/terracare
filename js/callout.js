de7248464080e315068b90568ba71df099cf3b28
quattromani <max@maxquattromani.com>
2014-07-30 15:07:33 -0600

    temporarily break callouts for display and decision making purposes

--- js/callout.js
+++ js/callout.js
@@ -2,17 +2,17 @@
     Call Out -- Version: 0.2.1
     ========================================================================== */
 
-    (function($) {
-      $.fn.callOutBox = function() {
-        if($(window).width() >= mediumBreakPoint){
-        	$('.callout').hover(function(){
-            $('.callout-message').slideDown('fast');
-          },
-          function(){
-            $('.callout-message').slideUp('slow');
-          });
-        }
-      }
+    // (function($) {
+    //   $.fn.callOutBox = function() {
+    //     if($(window).width() >= mediumBreakPoint){
+    //     	$('.callout').hover(function(){
+    //         $('.callout-message').slideDown('fast');
+    //       },
+    //       function(){
+    //         $('.callout-message').slideUp('slow');
+    //       });
+    //     }
+    //   }
 
-    }(jQuery));
-    $('.callout').callOutBox();
+    // }(jQuery));
+    // $('.callout').callOutBox();
