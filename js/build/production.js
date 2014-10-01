3b986ebbd94a6ef6ee38b71bb38c02e5ca0bd3bf
quattromani <max@maxquattromani.com>
2014-09-30 21:03:01 -0600

    chained http and https js target together

--- js/build/production.js
+++ js/build/production.js
@@ -276,7 +276,7 @@ $(function() {
 $('.getYear').getYear();
 
 // Open all external links in a new window
-$('a[href^="http://"]').attr('target','_blank');
+$('a[href^="http://"], a[href^="https://"]').attr('target','_blank');
 
 /* ==========================================================================
     Modal -- Version: 1.9.0.0 - Updated: 4/28/2014
--- js/main.js
+++ js/main.js
@@ -23,4 +23,4 @@ $(function() {
 $('.getYear').getYear();
 
 // Open all external links in a new window
-$('a[href^="http://"]').attr('target','_blank');
+$('a[href^="http://"], a[href^="https://"]').attr('target','_blank');
