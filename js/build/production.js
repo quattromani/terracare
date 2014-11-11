9b602ca2ffa5f592bb00431228e3ae45413913c4
quattromani <max@maxquattromani.com>
2014-11-10 17:28:38 -0700

    added spanish google doc for carerras

--- js/build/production.js
+++ js/build/production.js
@@ -142,6 +142,62 @@ $('.progress').progressButton();
 
 });
 
+$.getJSON('https://spreadsheets.google.com/feeds/list/1IT57mBl8Eu8r-yCRt8srnxYb9nAtp57U1ZpHq5nosY0/od6/public/values?alt=json', function(data) {
+
+  for (i = 0; i < data.feed.entry.length; i++) {
+    var state = data.feed.entry[i]['gsx$state']['$t'];
+    var title = data.feed.entry[i]['gsx$title']['$t'];
+
+      careers = $('<ul class="bulleted"/>');
+      careers.append('<li>' + title + '</li>');
+
+      if (state == 'California') {
+        $('.california-list').append(careers);
+      }
+
+      if (state == 'Colorado') {
+        $('.colorado-list').append(careers);
+      }
+
+      if (state == 'Texas') {
+        $('.texas-list').append(careers);
+      }
+
+      if (state == 'Utah') {
+        $('.utah-list').append(careers);
+      }
+  }
+
+});
+
+$.getJSON('https://spreadsheets.google.com/feeds/list/1CnZNSuuKnXgiDRU3I-FkcM8xXvo_Nxpj-zuo4iwyqxE/od6/public/values?alt=json', function(data) {
+
+  for (i = 0; i < data.feed.entry.length; i++) {
+    var state = data.feed.entry[i]['gsx$state']['$t'];
+    var title = data.feed.entry[i]['gsx$title']['$t'];
+
+      careers = $('<ul class="bulleted"/>');
+      careers.append('<li>' + title + '</li>');
+
+      if (state == 'California') {
+        $('.esp-california-list').append(careers);
+      }
+
+      if (state == 'Colorado') {
+        $('.esp-colorado-list').append(careers);
+      }
+
+      if (state == 'Texas') {
+        $('.esp-texas-list').append(careers);
+      }
+
+      if (state == 'Utah') {
+        $('.esp-utah-list').append(careers);
+      }
+  }
+
+});
+
 /*!
  * jQuery Cookie Plugin v1.4.1
  * https://github.com/carhartl/jquery-cookie
